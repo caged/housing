60cabd0e3ce8c03ec0b41098f9b18c89efeab5ad
Justin Palmer <justin@github.com>
2015-02-18 14:14:19 -0800

    render buildings

--- http/app.js
+++ http/app.js
@@ -1,6 +1,6 @@
 var map = d3.select('.js-map'),
-    width = 800,
-    height = 800
+    width = 1000,
+    height = 1000
 
 var path = d3.geo.path()
   .projection(null)
@@ -9,11 +9,7 @@ var svg = map.append('svg')
   .attr('width', width)
   .attr('height', height)
 
-d3.json("data/sw.json", function(err, pdx) {
-  svg.append('path')
-    .datum(topojson.feature(pdx, pdx.objects.blockgroups))
-    .attr('class', 'blockgroups')
-    .attr('d', path)
+d3.json("data/c.json", function(err, pdx) {
 
   svg.append('path')
     .datum(topojson.feature(pdx, pdx.objects.parks))
@@ -23,5 +19,15 @@ d3.json("data/sw.json", function(err, pdx) {
   svg.append('path')
     .datum(topojson.feature(pdx, pdx.objects.streets))
     .attr('class', 'streets')
+    .attr('d', path)
+
+  svg.append('path')
+    .datum(topojson.feature(pdx, pdx.objects.buildings))
+    .attr('class', 'buildings')
+    .attr('d', path)
+
+  svg.append('path')
+    .datum(topojson.feature(pdx, pdx.objects.blockgroups))
+    .attr('class', 'blockgroups')
     .attr('d', path)
 })
