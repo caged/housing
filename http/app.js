d04c5a3bc1cb51e44983f0ff7029f1ad32bd1082
Justin Palmer <justin@github.com>
2015-02-22 11:23:12 -0800

    draw hillshade data to screen, clip at bounds

--- http/app.js
+++ http/app.js
@@ -1,3 +1,5 @@
+var TSPD = 'n'
+
 var map = d3.select('.js-map'),
     width = 800,
     height = 800
@@ -9,7 +11,9 @@ var svg = map.append('svg')
   .attr('width', width)
   .attr('height', height)
 
-d3.json("data/ne.json", function(err, pdx) {
+var defs = svg.append('defs')
+
+d3.json("data/" + TSPD + ".json", function(err, pdx) {
 // Hardcoded bg median income extents from the entire Portland area
 var extent = [1368.33492, 207549.87568],
     extentr = [1302.11322, 145312.92241]
@@ -18,6 +22,24 @@ var color = d3.scale.linear()
   .domain(extent)
   .range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
   .interpolate(d3.interpolateCubehelix)
+
+  defs.append('path')
+    .datum(topojson.merge(pdx, pdx.objects.blockgroups.geometries))
+    .attr('id', 'land')
+    .attr('d', path)
+
+  defs.append('clipPath')
+    .attr('id', 'clip')
+  .append('use')
+    .attr('xlink:href', '#land')
+
+  svg.append('g')
+    .attr('clip-path', 'url(#clip)')
+  .append('image')
+    .attr('xlink:href', "data/hillshade-" + TSPD + ".png")
+    .attr('width', width)
+    .attr('height', height)
+    .attr('transform', 'rotate(0.2)')
 
   svg.selectAll('.blockgroups')
     .data(topojson.feature(pdx, pdx.objects.blockgroups).features)
@@ -48,8 +70,8 @@ var color = d3.scale.linear()
     .attr('class', 'buildings')
     .attr('d', path)
 
-  svg.append('text')
-    .attr('class', 'label')
-    .attr('transform', 'translate(500, 585) rotate(-28.5)')
-    .text('Sandy Boulevard')
+  // svg.append('text')
+  //   .attr('class', 'label')
+  //   .attr('transform', 'translate(500, 585) rotate(-28.5)')
+  //   .text('Sandy Boulevard')
 })
